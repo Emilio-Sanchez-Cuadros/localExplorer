9d2aa5bc75b9414aef5318b7fd978dc35d569415
Emilio Sánchez <esancheznumero1@gmail.com>
2021-05-16 11:28:19 +0200

    working click on folder to explore its content

--- local-explorer/src/App.js
+++ local-explorer/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Explorer from './explorer';
+import Explorer2 from './explorer2';
+import Displayer from './displayer'
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -11,7 +13,8 @@ function App() {
     <div className="App">
       <header className="App-header">
       <BrowserRouter>
-        <Route path='/:path' exact component={Explorer}/>
+        <Route path='/' exact component={Explorer}/>
+        <Route path='/2' exact component={Displayer}/>
       </BrowserRouter>
         </header>
     </div>
