5c0e626f1a679ffbd819edd73d6ea400bc05b881
Emilio Sánchez <esancheznumero1@gmail.com>
2021-05-18 19:48:18 +0200

    pagination fixed, now all files are shown

--- api/local-explorer.js
+++ api/local-explorer.js
@@ -1,15 +1,26 @@
 const fs = require('fs');
 const express = require("express");
-const { json } = require('body-parser');
+// const { json } = require('body-parser');
 const server = express();
 var cors = require('cors')
-var router = express.Router();
+// var router = express.Router();
 
 
 server.get("/:path", (req, res) => {
   let { path } = req.params
   path = path.replace(/%2F/g,"/");
+  path = encodeURI(path);
+  path = path.replace(/%20/g, " ");
   console.log(path);
+
+  if (path.search('Archivos de programa')) {
+    path = path.replace('Archivos de programa', 'Program Files')
+  } else if (path.search('Usuarios')) {
+    path = path.replace('Usuarios', 'Users')
+  } else if (path.search('Archivos comunes')) {
+    path = path.replace('Archivos comunes', 'Common Files')
+  }
+
   try {
     fs.readdir(`${path}`, function (err, files) {
       if (err)
